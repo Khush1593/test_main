54a0df9447ff1be5ef9417a397001dc05d86484f
Harsh Butani <harshbutani2004@gmail.com>
2024-04-18 14:50:01 +0530

    change in test harsh 2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -20,6 +20,8 @@ function App() {
       <h1>"sss patel</h1>
       <h1>"divya patel</h1>
       <h1>"divya patel</h1>
+      <h1>"change in test harsh"</h1>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
